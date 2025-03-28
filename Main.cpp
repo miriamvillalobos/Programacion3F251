11dd38181ed5b5dc45ffbc3909c832b30914f2de
miriamvillalobos <a24110230@live.ceti.mx>
2025-03-28 01:59:54 +0000

    practica1

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,24 @@
+#include "Serie.hpp"
 #include<iostream>
-#include"Foco.hpp"
+#include<ftxui/dom/element.hpp>
+#include<ftxui/screen/screen.hpp>
+
+using namespace ftxui;
+
 
 int main(int argc, char const*argv[])
 {
-  Foco foco1;
-  foco1.Encender();
-  std::cout
-    <<"Foco1:"
-    <<foco1.LeerEstado()
-    <<std::endl;
-  std::cout
-    <<"argc:"
-    <<argc
-    <<std::endl;
-  return 0;
-}
+  Screen pantalla = Screen::Create{
+    Dimension::Full(),
+    Dimension::Full()
+
+  };
+
+
+  auto document = ftxui::hbox({
+    ftxui::vbox(spiner(1,1))
+  });
+
+  ftxui::Render(pantalla,document);
+
+  return EXIT_SUCCESS;}
